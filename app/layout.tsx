97ed28a059de07f1c69a2e4428c59fe880424cb2
Manjunath G <modimanju2019@gmail.com>
2023-11-16 15:05:29 +0530

    build: :bento: Added assets & layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,47 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverFlow",
+  description:
+    "🚀 Welcome to DevOverflow - your go-to platform for developer collaboration and problem-solving, powered by Next.js 14!",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements:{
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover:text-primary-500'
+      }
+    }}>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
